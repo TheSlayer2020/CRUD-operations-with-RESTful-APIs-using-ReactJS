07116d0215af31e95be17bd00758cc55c1bdcc57
TheSlayer2020 <occ.crausus.joshua@gmail.com>
2023-11-13 12:54:57 +0800

    first commit

---
+++ src/app/components/ReadItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ReadItems = ({ items, onUpdate, onDelete }) => {
+ return (
+   <div>
+     <h2>ALL ITEMS</h2>
+     <ul>
+       {items.map((item) => (
+         <li key={item.id}>
+           {item.name} - {item.description}
+           <button onClick={() => onUpdate(item)}>Edit</button>
+           <button onClick={() => onDelete(item.id)}>Delete</button>
+         </li>
+       ))}
+     </ul>
+   </div>
+ );
+};
+
+
+export default ReadItems;
